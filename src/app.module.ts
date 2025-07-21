75079efe463e66473b0d551f357991b42de5902c
Daniels <42338092+DanielsHaitovs@users.noreply.github.com>
2025-07-21 08:54:22 +0300

    init project, docker is not completed

---
+++ src/app.module.ts
@@ -0,0 +1,69 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { TraceMiddleware } from '@/middleware/tracing.middleware';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ThrottlerModule, ThrottlerModuleOptions } from '@nestjs/throttler';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AppController } from './modules/app.controller';
+
+@Module({
+  controllers: [AppController],
+  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      // eslint-disable-next-line sonarjs/function-return-type
+      useFactory: (configService: ConfigService): ThrottlerModuleOptions => {
+        const ttl = parseInt(
+          configService.get<string>('THROTTLE_TTL') ?? '60',
+          10,
+        );
+        const limit = parseInt(
+          configService.get<string>('THROTTLE_LIMIT') ?? '5',
+          10,
+        );
+
+        return {
+          throttlers: [
+            {
+              ttl,
+              limit,
+            },
+          ],
+        };
+      },
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DATABASE_HOST') ?? 'localhost',
+        port: parseInt(
+          configService.get('PRODUCT_DATABASE_PORT') ?? '5433',
+          10,
+        ),
+        username: configService.get('DATABASE_USERNAME') ?? 'postgres',
+        password: configService.get('DATABASE_PASSWORD') ?? 'postgres',
+        database: configService.get('DATABASE_NAME') ?? 'postgres',
+        synchronize: configService.get('DATABASE_SYNCHRONIZE') === 'true',
+        logging: configService.get('DATABASE_LOGGING') === 'true',
+        entities: [`${__dirname}/**/*.entity{.ts,.js}`],
+        migrations: [`${__dirname}/migrations/*{.ts,.js}`],
+        autoLoadEntities: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(TraceMiddleware).forRoutes('*');
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,63 @@
+import { EntityNotFoundFilter } from '@/common/error/entity-not-found.filter';
+import { swaggerSetupOptions } from '@/config/swagger.config';
+import { LoggingInterceptor } from '@/interceptors/logging.interceptor';
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import { AppModule } from './app.module';
+
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalInterceptors(new LoggingInterceptor());
+
+  app.useGlobalFilters(new EntityNotFoundFilter());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: false, // 👈 critical
+      },
+      forbidUnknownValues: true, // 👈 also critical
+      validateCustomDecorators: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('MakeEasyCommerce User API')
+    .setDescription('E-commerce platform User API documentation')
+    .setVersion('1.0')
+    .addTag('App', 'Health check and basic operations')
+    .addTag('Auth', 'Auth in management operations')
+    .addTag('Me', 'Authenticated user session in management operations')
+    .addTag('Users', 'User management operations')
+    .addTag('Users Roles', 'User Roles management operations')
+    .addTag('Departments', 'Departments management operations')
+    .addTag('Roles', 'Roles management operations')
+    .addTag('Permissions', 'Permissions management operations')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'JWT-auth',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document, swaggerSetupOptions);
+
+  const port = Number(process.env.API_PORT) || 3000;
+  await app.listen(port);
+}
+
+void bootstrap();
